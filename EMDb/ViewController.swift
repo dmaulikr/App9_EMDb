25c8e54f358afb0d271918cf96d3928a7f969393
MIGUEL DIAZ RUBIO <miguel@macbookpro.local>
2016-10-02 18:11:19 +0200

    Initial Commit

---
+++ EMDb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMDb
+//
+//  Created by MIGUEL DIAZ RUBIO on 2/10/16.
+//  Copyright © 2016 Miguel Díaz Rubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
